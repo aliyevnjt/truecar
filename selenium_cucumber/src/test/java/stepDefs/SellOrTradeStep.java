8fc316697652b1e4ea005a6949e1118df3edf077
unknown <emine.dogan@gmail.com>
2019-12-05 21:32:29 -0500

    testing

---
+++ selenium_cucumber/src/test/java/stepDefs/SellOrTradeStep.java
@@ -0,0 +1,32 @@
+package stepDefs;
+
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.*;
+import pages.SellOrTradePage;
+import utils.SingletonBrowser;
+
+public class SellOrTradeStep {
+
+	SingletonBrowser sb = SingletonBrowser.getSingletonBrowser();
+	WebDriver driver = sb.getDriver();
+	
+	SellOrTradePage st = new SellOrTradePage (driver);
+	
+	
+	
+	@Given("User clicks on Sell or Trade button")
+	public void user_clicks_on_Sell_or_Trade_button() {
+	    st.sellortradebutton.click();
+	}
+
+	@Given("Verify button {string}")
+	public void verify_button(String string) {
+	   
+	}
+
+
+
+	
+	
+}
